c642f2380f022b03311af22112df68c632fd8230
rhod <rhodwilliams@hotmail.com>
2017-03-19 16:57:24 +0000

    add marker to map

--- src/components/gettingHere/gettingHere.js
+++ src/components/gettingHere/gettingHere.js
@@ -6,19 +6,28 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Segment from '../segment/Segment';
 import GoogleMap from 'google-map-react';
-import styles from './gettingHere.css'
+import styles from './gettingHere.css';
+import {googleMapsLink} from '../../data/data';
+
+const marker = () => (
+    <div lat={51.210111} lng={-0.516389}>here</div>
+)
+
 
 const BigDay = () => (
     <Segment title="Getting Here">
         <section>
             <p>Parking is limited but the house is a short taxi ride from Guildford station.</p>
-            <p>Show your taxi driver this map so he can find us.</p>
+            <p><a href={googleMapsLink} target="_blank" rel="noopener noreferrer">Open GU48RD in Maps</a> or show your taxi driver this map so he can find us.</p>
         </section>
         <div className={styles.map}>
             <GoogleMap
                 // apiKey={YOUR_GOOGLE_MAP_API_KEY} // set if you need stats etc ...
                 center={[51.210111, -0.516389]}
-                zoom={9}>
+                zoom={11}
+                layerTypes={['TrafficLayer', 'TransitLayer']}
+                >
+                {marker()}
             </GoogleMap>
         </div>
     </Segment>
