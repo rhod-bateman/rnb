17e90b8de32696f13a1999435dc6fbadf56cbee7
rhodwilliams <rhodwilliams@hotmail.com>
2017-04-15 19:28:23 +0100

    revert changes. copy only dist folder to wwwroot

--- deploy/azure-deploy.sh
+++ deploy/azure-deploy.sh
@@ -130,30 +130,11 @@ if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
 fi
 
 # 5. KuduSync
-#if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
-#  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
-#  exitWithMessageOnError "Kudu Sync failed"
-#fi
+if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
+  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
+  exitWithMessageOnError "Kudu Sync failed"
+fi
 
-:: 5. Do KuduSync BEFORE INSTALLING PRODUCTION DEPENDANCIES
-echo ======= Kudu Syncing: Starting at %TIME% ======= 
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist;%DEPLOYMENT_SOURCE%/package.json" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.vscode;node_modules;src;deploy;typings;.bowerrc;.deployment;.gitignore;bower.json;webpack.config.*;deploy.cmd;gulpfile.js;tsconfig.json;tsd.json;.hg;.deployment;deploy.cmd;*.xml;*.yml"
-  IF !ERRORLEVEL! NEQ 0 goto error
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/package.json" -t "%DEPLOYMENT_TARGET%"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-echo ======= Kudu Syncing: Finished at %TIME% =======
-
-:: 6. Install npm packages at DEPLOYMENT_TARGET 
-echo =======  Installing npm packages: Starting at %TIME% ======= 
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-echo =======  Installing npm packages: Finished at %TIME% =======
 
 ##################################################################################################################################
 echo "Finished successfully."
