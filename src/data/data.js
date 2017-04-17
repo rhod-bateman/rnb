9a2f6057fc98274bb763ef16d3b25bcade93d6ad
rhodwilliams <rhodwilliams@hotmail.com>
2017-04-17 12:28:54 +0100

    change maps link

--- src/data/data.js
+++ src/data/data.js
@@ -12,5 +12,5 @@ export const timetable = {
     "11:30":"tie round head dance time"
 };
 
-export const googleMapsLink="https://www.google.co.uk/maps/place/51%C2%B012'36.4%22N+0%C2%B030'59.0%22W/@51.2101173,-0.518579,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d51.210114!4d-0.516385";
+export const googleMapsLink="https://maps.google.co.uk/maps/place/51%C2%B012'36.4%22N+0%C2%B030'59.0%22W/@51.2101173,-0.518579,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d51.210114!4d-0.516385";
 
