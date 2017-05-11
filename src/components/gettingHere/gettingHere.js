33ce882ed3af93a0607c57f3c58d29e4577c6a6c
rhod <rhodwilliams@hotmail.com>
2017-05-11 22:34:29 +0100

    shrink map

--- src/components/gettingHere/gettingHere.js
+++ src/components/gettingHere/gettingHere.js
@@ -8,12 +8,13 @@ import Segment from '../segment/Segment';
 import styles from './gettingHere.css';
 import {googleMapsLink} from '../../data/data';
 import {Map, Marker} from '../map/map';
+import NewTabLink from '../utils/newTabLink';
 
 const BigDay = () => (
     <Segment title="Getting Here..." name="journey">
         <section>
             <p>Parking is limited but the house is a short taxi ride from Guildford station.</p>
-            <p><a href={googleMapsLink} target="_blank" rel="noopener noreferrer">Open GU48RD in Maps</a> or show your taxi driver this map so he can find us.</p>
+            <p><NewTabLink href={googleMapsLink}>Open GU48RD in Maps</NewTabLink> or show your taxi driver this map so he can find us.</p>
         </section>
         <div className={styles.map}>
             <Map center={[51.210111, -0.516389]} zoom={11}>
