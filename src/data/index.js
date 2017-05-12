59cd5ef0380ab04bd81d06d9e0eb4a5cab5b821f
rhod <rhodwilliams@hotmail.com>
2017-05-12 22:19:32 +0100

    try amazon links

--- src/data/index.js
+++ src/data/index.js
@@ -20,10 +20,10 @@ export const spotifyLink="https://open.spotify.com/user/rhod-williams/playlist/3
 
 export const bookingLink="https://www.booking.com/searchresults.html?checkin_month=7&checkin_monthday=15&checkin_year=2017&checkout_month=7&checkout_monthday=16&checkout_year=2017&city=-2592457&class_interval=1&dest_id=-2611995&dest_type=city&dtdisc=0&group_adults=2&group_children=0&inac=0&index_postcard=0&label_click=undef&mih=0&no_rooms=1&postcard=0&raw_dest_type=city&room1=A%2CA&sb_price_type=total&search_selected=1&src_elem=sb&ss=Wonersh%2C%20Surrey%2C%20United%20Kingdom&ss_all=0&ss_raw=Wonersh&ssb=empty&sshis=0&ssne_untouched=Chilworth&=&order=distance_from_landmark&dst_landmark=cc";
 
-export const cushion1Link="http://www.amazon.com/gp/mas/dl/android?asin=B00TVSI7UW/";
+export const cushion1Link="https://www.amazon.co.uk/gp/product/B00TVSI7UW";
 export const cushion1Image="https://images-na.ssl-images-amazon.com/images/I/610T7Saws-L._SL1500_.jpg";
 
-export const cushion2Link="http://www.amazon.co.uk/gp/mas/dl/android?asin=B01N1I6AEB/";
+export const cushion2Link="https://www.amazon.co.uk/gp/product/B01N1I6AEB";
 export const cushion2Image="https://images-na.ssl-images-amazon.com/images/I/71LSU5loefL._SX522_.jpg";
 
 export const photosAlbum="https://goo.gl/photos/jeV1x7ME11sg5xdf9";
