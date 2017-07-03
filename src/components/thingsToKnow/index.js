d83a994a3b6c111c5d78e24aa3996683425a3310
rhodwilliams <rhodwilliams@hotmail.com>
2017-07-03 18:36:53 +0100

    Update index.js

--- src/components/thingsToKnow/index.js
+++ src/components/thingsToKnow/index.js
@@ -5,26 +5,33 @@
  * Created by qtj929 on 20/12/2016.
  */
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import styles from './thingsToKnow.css';
 import Segment from '../segment/Segment';
-import NewTablink from '../utils/newTabLink';
-import { giftLink } from '../../data/'
+import NewTabLink from '../utils/newTabLink';
+import Image from '../utils/image';
+import {cushion1Link, cushion1Image, cushion2Link, cushion2Image} from '../../data/index';
 
-const Gifts = () => (
-    <Segment title="Gifts" name="gifts">
+const ThingsToKnow = () => (
+    <Segment title="Things you should know" name="thingsToKnow">
         <section>
-            <p>We've been living together for enough years to have everything we need (and probably more).</p>
-            <p>Please don't feel that you need to give us anything, we're happy enough that you're coming.</p>
-            <p>But as people keep asking us for ideas, here are a few we thought of...</p>
+            <h3>Dress Code</h3>
+            <p>Whatever makes you feel most fabulous</p>
+            <p>Please bear in mind that you will be walking though leaf litter in the woods and sitting on a picnic blanket later on.</p>
         </section>
-        <section>
+
+        <section><h3>What to bring:</h3>
             <ul>
-                <li>There are a 1001 good causes in the world. You could pick your favourite and make a donation for us</li>
-                <li>If you'd rather spend your money on us, life is all about experiences.</li>
-                <li>Or if you really want to give us a thing, we have set up a <NewTablink href={giftLink} > John Lewis gift list </NewTablink>. </li>
+                <li>Suntan lotion</li>
+                <li>Suitable footwear for woodland and lawns.</li>
+                <li>Something warm for when the sun goes down.</li>
+                <li>If you want a cushion for the picnic, amazon has some cushion-chairs;
+                    <NewTabLink href={cushion1Link} > <Image src={cushion1Image} className={styles.amazonImage} /></NewTabLink>
+                    <NewTabLink href={cushion2Link} > <Image src={cushion2Image} className={styles.amazonImage} /></NewTabLink>
+                </li>
             </ul>
         </section>
     </Segment>
 );
 
-
-export default Gifts;
+export default ThingsToKnow;
