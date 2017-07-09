c05a609bba4392de9ee174d889f87d2de6c04b29
rhodwilliams <rhodwilliams@hotmail.com>
2017-07-09 22:30:27 +0100

    Fix rendering error

--- src/components/Html.js
+++ src/components/Html.js
@@ -3,23 +3,29 @@
  */
 import React, { PropTypes } from 'react';
 
+const getStateHtml = state => {
+    const stateString = {__html:`<script>try{window.__PRELOADED_STATE__ = (${state});} catch(err) { console.log(err); }</script>`};
+    console.log(stateString);
+    return stateString;
+}
+
 const Html = ({ bundle, children, title, state, css }) => (
     <html lang="en">
-        <head>
-            <meta charSet="utf-8" />
-            {title && <title>{title}</title>}
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link href="https://fonts.googleapis.com/css?family=Mystery Quest|Rancho" rel="stylesheet"/>
-            <link href={css} rel="stylesheet"/>
-        </head>
-        <body>
-            <div id="root" >
-                {children}
-            </div>
-            <script type="text/javascript" src={bundle} />
-            {state && <script type="text/javascript">__window.PRELOADED_STATE={state}</script>}
-        </body>
+    <head>
+        <meta charSet="utf-8"/>
+        {title && <title>{title}</title>}
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="https://fonts.googleapis.com/css?family=Mystery Quest|Rancho" rel="stylesheet"/>
+        <link href={css} rel="stylesheet"/>
+    </head>
+    <body>
+    <div id="root">
+        {children}
+    </div>
+    <script type="text/javascript" src={bundle}/>
+    <div dangerouslySetInnerHTML={getStateHtml(state)}></div>
+    </body>
     </html>
-);
+)
 
 export default Html;
